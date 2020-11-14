1fa524140df1a55efc424b30566eb290551ba38c
Shenge01 <nkzmlbuthelezi@gmail.com>
2020-11-14 14:37:15 +0200

    update the unit test response

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,4 +1,5 @@
 from ..model_package.classififer_titanic import classifier_titanic_model
+import ast
 
 def test_health_endpoint_returns_200(flask_test_client):
 	# When
@@ -6,11 +7,13 @@ def test_health_endpoint_returns_200(flask_test_client):
 
 	# Then
 	assert response.status_code == 200
-	
+
+# Will need to look into the response object of this unit test in the future
 def test_prediction_endpoint_returns_prediction(flask_test_client):
 	# When 
 	test_json_data = {"Pclass":3, "Name":"Kelly, Mr. James","Sex":"male", "Age":34, "SibSp":0, "Parch":0,"Ticket":330911, "Fare":7.8292, "Cabin":"B45", "Embarked":"Q"}
 
 	response = flask_test_client.post('/v1/predict/xgboostclassifier', json=test_json_data)
-
-	assert response.status_code == 200
+	response = ast.literal_eval(response.get_data(True)).values()
+	
+	assert list(response)[0] == 0
